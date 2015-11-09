66367bf840753e89bc9df1085ed0e982b9c25b8b
Halyna Kotsiuban <Halyna Kotsiuban>
2015-11-10 00:03:20 +0200

    first one

---
+++ Delegate1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class Program
+    {
+        public static void RunDelegate(Action<int> action, int count)
+        {
+            while (count > 0)
+            {
+                action(count);
+                count--;
+            }
+        }
+
+        public static void WriteToConsole (int count)
+        {
+            Console.WriteLine(count);
+        }
+
+        static void Main(string[] args)
+        {
+            RunDelegate(WriteToConsole, 2);
+            Console.ReadLine();
+        }
+    }
+}
